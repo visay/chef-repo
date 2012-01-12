2f6cfd397f900fbac73bfc2a7cd718861cff3b2d
Fabien Udriot <fudriot@omic.ch>
2012-01-12 14:47:53 +0700

    config

--- roles/base.rb
+++ roles/base.rb
@@ -1,6 +1,10 @@
 name "base"
     description "base role applied to all nodes."
-    #run_list()
+    
+    run_list "recipe[apt]"
+    
+    #ntp, git, svn, curl, wget, openssh
+    
     # Attributes applied if the node doesn't have it set already.
     #default_attributes()
     # Attributes applied no matter what the node has set already.
