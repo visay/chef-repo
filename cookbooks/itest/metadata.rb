d5c8b77ea046299501fc345a14795453e743d4c4
Fabien Udriot <fudriot@omic.ch>
2012-01-12 12:35:32 +0700

    Remove unwanted cookbook

--- cookbooks/itest/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Web Essentials"
-maintainer_email "fabien@web-essentials.asia"
-license          "Apache 2.0"
-description      "Installs/Configures itest"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
