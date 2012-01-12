cbd8fb90867a8505d98d00041995089cb393a051
Fabien Udriot <fudriot@omic.ch>
2012-01-12 18:30:26 +0700

    config

--- roles/base.rb
+++ roles/base.rb
@@ -2,7 +2,7 @@ name "base"
     description "base role applied to all nodes."
     
     #, git, svn, curl, wget, openssh
-    run_list "recipe[apt]", "recipe[ntp]"
+    run_list "recipe[apt]", "recipe[ntp]", "recipe[base]"
     
     # Attributes applied if the node doesn't have it set already.
     default_attributes "ntp" => {
--- roles/web.rb
+++ roles/web.rb
@@ -5,6 +5,7 @@ name "web"
 	# @doc http://wiki.opscode.com/display/chef/Roles
 	
     # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+    run_list "recipe[apache2]", "recipe[php]"
     #run_list "recipe[apache2]", "recipe[apache2::mod_ssl]", "role[monitor]"
 
 	#env_run_lists "prod" => ["recipe[apache2]"], "staging" => ["recipe[apache2::staging]"]
