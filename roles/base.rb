7a1c421e0603693d08aa97ed970550ebddd2259c
Christian Trabold <info@christian-trabold.de>
2012-08-24 16:53:39 +0700

    Reformat file

--- roles/base.rb
+++ roles/base.rb
@@ -2,23 +2,27 @@ name "base"
 description "base role applied to all nodes."
 
 #, git, svn, curl, wget, openssh
-run_list "recipe[apt]",
-         "recipe[ntp]",
-         "recipe[iptables]",
-         "recipe[base]",
-         "recipe[base::firewall]",
-         "recipe[zsh]",
-         "recipe[oh-my-zsh]"
-
-# Attributes applied if the node doesn't have it set already.
-default_attributes
+run_list(
+  "recipe[apt]",
+  "recipe[ntp]",
+  "recipe[iptables]",
+  "recipe[base]",
+  "recipe[base::firewall]",
+  "recipe[zsh]",
+  "recipe[oh-my-zsh]"
+)
 
 # -> should be rather override_attributes
 # Attributes applied no matter what the node has set already.
-override_attributes "ntp" => {
-    "servers" => ["ch.pool.ntp.org", "pool.ntp.org", "ntp.ubuntu.com"]
-}, "ohmyzsh" => {
+override_attributes(
+  "ntp" => {
+    "servers" => [
+      "ch.pool.ntp.org",
+      "pool.ntp.org",
+      "ntp.ubuntu.com"
+    ]
+  },
+  "ohmyzsh" => {
     "theme" => "fletcherm"
-}
-
-
+  }
+)
