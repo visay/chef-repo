ed2feccc8dbb17538d5190e119eb0714ebebd793
Christian Trabold <info@christian-trabold.de>
2012-08-24 22:39:50 +0700

    Update description for base role

--- roles/base.rb
+++ roles/base.rb
@@ -1,5 +1,5 @@
 name "base"
-description "base role applied to all nodes."
+description "IMPORTANT: Never apply this base role directly to a node. Use the OS specific roles like 'ubuntu'. The run_list of this role is applied to ALL nodes and creates a base for further recipes."
 
 run_list(
   "recipe[zip]",
@@ -7,6 +7,15 @@ run_list(
   "recipe[curl]",
   "recipe[subversion]",
   "recipe[ntp]"
+)
+
+default_attributes(
+  "chef_client" => {
+    "server_url" => "https://chef-api.web-essentials.asia"
+  },
+  "chef_handler" => {
+    "handler_path" => "/var/chef/handlers"
+  }
 )
 
 override_attributes(
