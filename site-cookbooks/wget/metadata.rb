dd2e49996127830084659a522866045de170dbf6
Christian Trabold <info@christian-trabold.de>
2012-08-24 18:23:08 +0700

    Add site-cookbooks for wget and zip
    The goal of this commit is to remove the "base" cookbook in
    "site-cookbooks" and to make the cookbook structure more transparent.

---
+++ site-cookbooks/wget/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "dkd Internet Service GmbH"
+maintainer_email "christian.trabold@dkd.de"
+license          "MIT"
+description      "Installs/Configures wget"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
